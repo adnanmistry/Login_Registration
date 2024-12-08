0da2e74d64230850d01bdc58214ef484b46c0f19
DESKTOP-2P1DUCO\Admin <mistryadnan2000@gmail.com>
2024-12-08 19:05:12 +0530

    updated code

--- Personal_Blog/Personal Blog/Pages/Contact/Index.cshtml.cs
+++ Personal_Blog/Personal Blog/Pages/Contact/Index.cshtml.cs
@@ -1,12 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace Personal_Blog.Pages.Contact
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public ContactModel Contact { get; set; } = new ContactModel();
+
+        public bool MessageSent { get; set; }
+
         public void OnGet()
         {
+            // Initialize the Contact property if needed
+           
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Process the contact form submission
+            // For example, you could send an email, save the message to a database, etc.
+
+            MessageSent = true;
+            return Page();
+        }
+    }
+
+    public class ContactModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
     }
 }
